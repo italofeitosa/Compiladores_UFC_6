f6b67c5a2612efb764b3fb74410d04444ed8104a
italofeitosa <itinhofeitosa@gmail.com>
2015-06-18 10:30:03 -0300

    commit inicial da parte 5 - alocação de registradores

---
+++ src/regAlloc/MoveList.java
@@ -0,0 +1,9 @@
+package regAlloc;
+
+public class MoveList {
+   public graph.Node src, dst;
+   public MoveList tail;
+   public MoveList(graph.Node s, graph.Node d, MoveList t) {
+	src=s; dst=d; tail=t;
+   }
+}
